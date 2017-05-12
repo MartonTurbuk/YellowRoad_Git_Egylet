7871a4c0d8ecf0aca7276b081c2a60f78ed3c526
veliczkya <veliczky.a@gmail.com>
2017-05-12 12:08:11 +0200

    - két display fv

---
+++ Star_Wars_Display.js
@@ -0,0 +1,38 @@
+
+var ObjMasterDiv=document.querySelector('#');
+var ObjDetailDiv
+//StarWars
+var tobeDisplayed=["name","height","mass","hair_color","skin_color","eye_color","birth_year","gender"];
+
+
+function IdGenerator(){
+    for(var i=0;i<StarWars.length;i++){
+        StarWars[i].id=i;
+    }
+}
+
+function DisplayMaster(){
+    var Table=document.createElement("TABLE");
+    for(var i=0;i<StarWars.length;i++){
+        var Row=document.createElement("TR");
+        var Data=document.createElement("TD");
+        Data.innerHTML=StarWars[i].name;
+        Row.appendChild(Data);
+        Table.appendChild(Row);
+    }
+    ObjMasterDiv.appendChild(Table);
+}
+
+function DisplayDetails(){
+    var Table=document.createElement("TABLE");
+    for(var i=0;i<StarWars.length;i++){
+        var Row=document.createElement("TR");
+        for(var j=0;j<tobeDisplayed.length;j++){
+            var Data=document.createElement("TD");
+            Data.innerHTML=StarWars[i][tobeDisplayed[j]];
+        }
+        Row.appendChild(Data);
+        Table.appendChild(Row);
+    }
+    ObjMasterDiv.appendChild(Table);
+}
