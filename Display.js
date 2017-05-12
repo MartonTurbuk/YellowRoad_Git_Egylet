c9614680b63069c31697a440ba6458af15867d93
veliczkya <veliczky.a@gmail.com>
2017-05-12 12:54:34 +0200

    major

--- Display.js
+++ Display.js
@@ -5,6 +5,8 @@ var ObjDetailDiv=document.querySelector('#charview');
 //kiirandó adatok listája
 var tobeDisplayed=["name","height","mass","hair_color","skin_color","eye_color","birth_year","gender"];
 
+var DataMaster=[];
+
 //beszédes az elnevezés, csak ha esetleg szükség lenne rá, id-zi a táblát
 function IdGenerator(){
     for(var i=0;i<StarWars.length;i++){
@@ -16,9 +18,14 @@ function IdGenerator(){
 function DisplayMaster(){
         var Table=document.createElement("TABLE");
     for(var i=0;i<StarWars.length;i++){
+        DataMaster.push(' ');
         var Row=document.createElement("TR");
-        var Data=document.createElement("TD");
-        Data.innerHTML=StarWars[i].name;
+        var DataMaster[i]=document.createElement("TD");
+        DataMaster[i].innerHTML=StarWars[i].name;
+        DataMaster[i].setAttribute('id',i);
+        DataMaster[i].addEventListener(click,function(event){
+            DisplayDetails(event.target.id);
+        })
         Row.appendChild(Data);
         Table.appendChild(Row);
     }
@@ -26,13 +33,13 @@ function DisplayMaster(){
 }
 
 // a tobeDisplayed tömbbe írt adatokat listázza ki
-function DisplayDetails(){
-    var oncklickValue=event.target.value;
+function DisplayDetails(incomingID){
+    ObjDetailDiv.innerHTML=" ";
     var Table=document.createElement("TABLE");
     var Row=document.createElement("TR");
     for(var j=0;j<tobeDisplayed.length;j++){
         var Data=document.createElement("TD");
-        Data.innerHTML=StarWars[oncklickValue][tobeDisplayed[j]];
+        Data.innerHTML=StarWars[incomingID][tobeDisplayed[j]];
         Row.appendChild(Data);
     }
     Table.appendChild(Row);
