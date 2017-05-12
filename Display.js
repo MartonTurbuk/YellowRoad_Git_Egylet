87c70a0a79d52925665312eb614fc98334f1a142
veliczkya <veliczky.a@gmail.com>
2017-05-12 13:08:54 +0200

    valami működik

--- Display.js
+++ Display.js
@@ -16,32 +16,38 @@ function IdGenerator(){
 
 //Master oldal megjelenítés, táblázat, for ciklusra kilistázza külön sorokba a name értékeket
 function DisplayMaster(){
+        strippedData();
         var Table=document.createElement("TABLE");
-    for(var i=0;i<StarWars.length;i++){
-        DataMaster.push(' ');
-        var Row=document.createElement("TR");
-        DataMaster[i]=document.createElement("TD");
-        DataMaster[i].innerHTML=StarWars[i].name;
-        DataMaster[i].setAttribute('id',i);
-        DataMaster[i].addEventListener(click,function(event){
-            DisplayDetails(event.target.id);
-        })
-        Row.appendChild(DataMaster[i]);
-        Table.appendChild(Row);
-    }
-    ObjMasterDiv.appendChild(Table);
+        for(var i=0;i<StarWars.length;i++){
+            DataMaster.push(' ');
+            var Row=document.createElement("TR");
+            DataMaster[i]=document.createElement("TD");
+            DataMaster[i].innerHTML=StarWars[i].name;
+            DataMaster[i].setAttribute('id',i);
+            DataMaster[i].addEventListener('click',function(event){
+                DisplayDetails(event.target.id);
+            })
+            Row.appendChild(DataMaster[i]);
+            Table.appendChild(Row);
+        }
+        ObjMasterDiv.appendChild(Table);
 }
 
 // a tobeDisplayed tömbbe írt adatokat listázza ki
 function DisplayDetails(incomingID){
     ObjDetailDiv.innerHTML=" ";
     var Table=document.createElement("TABLE");
-    var Row=document.createElement("TR");
+    
     for(var j=0;j<tobeDisplayed.length;j++){
+        var Row=document.createElement("TR");
+        var DataX=document.createElement("TD");
+        DataX.innerHTML=tobeDisplayed[j];
         var Data=document.createElement("TD");
         Data.innerHTML=StarWars[incomingID][tobeDisplayed[j]];
+        Row.appendChild(DataX);
         Row.appendChild(Data);
+        Table.appendChild(Row);
     }
-    Table.appendChild(Row);
+    
     ObjDetailDiv.appendChild(Table);
 }
