7263737cadf9e437220e327ee7c722337de8ff63
Gyuri <gyorgy.vargadudas@gmail.com>
2017-05-13 14:06:10 +0200

    rendeztem a kódot, remélhetőleg

--- Background_function.js
+++ Background_function.js
@@ -10,62 +10,58 @@ function nevSorbaRendez(a, b) {
   }
   return 0;
 }
-StarWars.sort(nevSorbaRendez);
 
 /*Karakterek magasság szerinti sorba rendezése, növekvő értékekkel*/
-StarWars.sort(function (a, b) {
+StarWars.sort(function(a, b) {
   return a.height - b.height;
 });
 
 /*Karakterek magasság szerinti sorba rendezése, csökkenő értékekkel*/
-StarWars.sort(function (a, b) {
+StarWars.sort(function(a, b) {
   return b.height - a.height;
 });
 
 /*Karakterek súly szerinti sorba rendezése, növekvő értékekkel*/
-StarWars.sort(function (a, b) {
+StarWars.sort(function(a, b) {
   return a.mass - b.mass;
 });
 
 /*Karakterek súly szerinti sorba rendezése, csökkenő értékekkel*/
-StarWars.sort(function (a, b) {
+StarWars.sort(function(a, b) {
   return b.mass - a.mass;
 });
 
-/*Karakterek csoportosítása nem szerint*/
-var genderMale = [];
-var genderFemale = [];
-var genderDroid = [];
-
-/* Nem szerinti tömbbe rendezés
-function filterMale(char) {
-  if (char.gender === "male") {
-    return true;
-  }
-}
-
-function filterMaleGender() {
-  genderMale = StarWars.filter(filterMale);
-}
-
-filterMaleGender();
-console.log(genderMale);
-*/
-
 /*General függvény, nem csoportosításra*/
 function generalFilterFunction(array, gender) {
-for (var i = 0; i < array.length; i++) {
-  if (array[i].gender == gender) {
-    switch (gender) {
-      case "male":
-        genderMale.push(array[i]);
-        break;
-      case "female":
-        genderFemale.push(array[i]);
-        break;
-      default:
-        genderDroid.push(array[i]);
+  var sortedByGender = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].gender == gender) {
+      sortedByGender.push(array[i]);
     }
   }
+  return sortedByGender;
 }
+
+var checkboxDiv = document.querySelector('#checkboxDiv');
+checkboxDiv.addEventListener('click', checkboxSelect);
+//listázza a csekkolt 1nemeket'
+function checkboxSelect() {
+  var filteredArray = [];
+  var male = document.querySelector('#Male');
+  var female = document.querySelector('#Female');
+  var droid = document.querySelector('#Droid');
+  if (male.checked === true) {
+    var males = generalFilterFunction(StarWars, 'male');
+    filteredArray = filteredArray.concat(males);
+  }
+  if (female.checked === true) {
+    var females = generalFilterFunction(StarWars, 'female');
+    filteredArray = filteredArray.concat(females);
+  }
+  if (droid.checked === true) {
+    var droids = generalFilterFunction(StarWars, 'droid');
+    /*console.log(droids);*/
+    filteredArray = filteredArray.concat(droids);
+  }
+  DisplayMaster(filteredArray);
 }
